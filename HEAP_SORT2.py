f88b58cac211a27dfebd9799b8ea3675af5c5396
sameerCoder <39183783+sameerCoder@users.noreply.github.com>
2018-10-23 14:02:40 +0530

    Add files via upload

---
+++ HEAP_SORT2.py
@@ -0,0 +1,13 @@
+from heapq import heappop,heappush,heapify
+
+heap=[]
+nums=[12,3,-2,6,8]
+
+#for num in numbs:
+ #heappush(heap,num)
+
+#while heap:
+    #print(heappop(heap))
+
+heapify(nums)
+print(nums)
